16f388638730fad4ee5d722cd8cc6ba307bf31fb
rgielen <rgielen@c35a2e9e-9649-4ca1-9090-790cbf53f57c>
2015-09-17 14:09:50 +0000

    first real maven project, including Eclipse ignores

---
+++ trunk/src/main/java/net/rgielen/maven/Foo.java
@@ -0,0 +1,14 @@
+package net.rgielen.maven;
+
+import org.apache.log4j.Logger;
+
+public class Foo {
+
+	static Logger LOG = Logger.getLogger(Foo.class);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LOG.debug("Hallo");
+	}
+
+}
